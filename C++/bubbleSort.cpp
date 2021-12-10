cfd98e38b753a7f340f6b5ff4dcb662b221af7d4
ZhouCihang <matthewzhou1128@gmail.com>
2021-12-10 18:41:32 +0800

    refine bubble sort

--- C++/bubbleSort.cpp
+++ C++/bubbleSort.cpp
@@ -6,27 +6,34 @@ int main()
 {
 
     int arr[] = {2, 4, 0, 5, 7, 1, 3, 8, 9};
-    int start = 0;
     int temp;
+
+    cout << "before bubble sort: " << endl;
+    for (int j = 0; j < sizeof(arr) / sizeof(arr[0]); j++)
+    {
+        cout << arr[j] << " ";
+    }
+    cout << endl;
 
     for (int j = 0; j < sizeof(arr) / sizeof(arr[0]) - 1; j++)
     {
         for (int i = 0; i < sizeof(arr) / sizeof(arr[0]) - j - 1; i++)
         {
-            if (arr[start] > arr[start + 1])
+            if (arr[i] > arr[i + 1])
             {
-                temp = arr[start];
-                arr[start] = arr[start + 1];
-                arr[start + 1] = temp;
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
             }
-            start++;
         }
-        for (int j = 0; j < sizeof(arr) / sizeof(arr[0]); j++)
-        {
-            cout << arr[j] << endl;
-        }
-        start = 0;
     }
 
+    cout << "after bubble sort: " << endl;
+    for (int j = 0; j < sizeof(arr) / sizeof(arr[0]); j++)
+    {
+        cout << arr[j] << " ";
+    }
+
+    cout << endl;
     return 0;
 }
