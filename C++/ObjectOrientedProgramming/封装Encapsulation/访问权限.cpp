b60ff738b315d1d8b8aad404efdb89c47a758cce
ZhouCihang <matthewzhou1128@gmail.com>
2021-12-26 16:29:13 +0800

    封装 访问权限

---
+++ C++/ObjectOrientedProgramming/封装Encapsulation/访问权限.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+    string m_Name;
+
+protected:
+    string m_Car;
+
+private:
+    int m_Password;
+
+public:
+    void func()
+    {
+        m_Name = "张三";
+        m_Car = "拖拉机";
+        m_Password = 123456;
+    }
+};
+
+int main()
+{
+
+    Person p1;
+
+    p1.m_Name = "李四";
+    p1.func();
+    return 0;
+}
