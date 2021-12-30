050f9f1b8d8a3e570c11672323289c201706fb89
ZhouCihang <matthewzhou1128@gmail.com>
2021-12-30 14:36:46 +0800

    类和对象 友元

---
+++ C++/ObjectOrientedProgramming/封装Encapsulation/友元.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Building
+{
+    friend void goodDay(Building *building);
+
+public:
+    Building()
+    {
+        m_SittingRoom = "Living room";
+        m_Bedroom = "Bedroom";
+    }
+
+public:
+    string m_SittingRoom;
+
+private:
+    string m_Bedroom;
+};
+
+void goodDay(Building *building)
+{
+
+    cout << building->m_SittingRoom << endl;
+    cout << building->m_Bedroom << endl;
+}
+
+void test01()
+{
+    Building bd;
+    goodDay(&bd);
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
