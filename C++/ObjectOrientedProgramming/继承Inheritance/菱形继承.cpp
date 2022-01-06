08e9de6d51d0ffc095a116ccc63bb3f572d93eeb
ZhouCihang <matthewzhou1128@gmail.com>
2022-01-06 16:35:04 +0800

    继承 菱形继承

---
+++ C++/ObjectOrientedProgramming/继承Inheritance/菱形继承.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+
+    int m_Age;
+};
+
+//利用虚继承 解决菱形继承问题，加上virtual
+class Sheep : virtual public Animal
+{
+};
+
+class Camel : virtual public Animal
+{
+};
+
+class SheepCamel : public Sheep, public Camel
+{
+};
+
+void test01(){
+    SheepCamel sc;
+    sc.Sheep::m_Age = 18;
+    sc.Camel::m_Age = 25;
+    cout << sc.Sheep::m_Age << endl;
+    cout << sc.Camel::m_Age << endl;
+    cout <<sc.m_Age<<endl;
+
+    //重复数据只需要有一份，菱形继承导致数据两份，资源浪费
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
