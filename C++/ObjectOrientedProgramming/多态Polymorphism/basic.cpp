6e5dffd27fdf857552234e4ae67cff0f2711babf
ZhouCihang <matthewzhou1128@gmail.com>
2022-01-06 16:35:22 +0800

    多态 基本语法

---
+++ C++/ObjectOrientedProgramming/多态Polymorphism/basic.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void speak()
+    {
+        cout << "Animal speak." << endl;
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void speak()
+    {
+        cout << "Cat speak." << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void speak()
+    {
+        cout << "Dog speak." << endl;
+    }
+};
+
+//地址早绑定，在编译阶段确定函数地址。
+//如果想让猫说话，地址不可以早绑定，在运行阶段绑定，晚绑定。加virtual
+void doSpeak(Animal &animal){ //Animal & animal = cat;
+    animal.speak();
+}
+
+
+void test01(){
+    Cat cat;
+    doSpeak(cat);
+
+    Dog dog;
+    doSpeak(dog);
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
