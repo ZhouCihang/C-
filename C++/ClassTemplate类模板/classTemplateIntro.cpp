367d6bbb9f8e07fde647afbd4df03bae4d937ccc
ZhouCihang <matthewzhou1128@gmail.com>
2022-01-26 16:38:14 +0800

    类模版 成员函数类外实现

--- C++/ClassTemplate类模板/classTemplateIntro.cpp
+++ C++/ClassTemplate类模板/classTemplateIntro.cpp
@@ -5,22 +5,66 @@ template <class NameType, class AgeType>
 class Person
 {
 public:
-    Person(NameType name, AgeType age){
+    Person(NameType name, AgeType age)
+    {
         this->m_Name = name;
         this->m_Age = age;
     }
 
-    void showInfo(){
-        cout << "Name: " << this->m_Name << " Age: " << this->m_Age<<endl;
+    void showInfo()
+    {
+        cout << "Name: " << this->m_Name << " Age: " << this->m_Age << endl;
     }
-    
+
     NameType m_Name;
     AgeType m_Age;
 };
 
-void test01(){
+template <class NameType, class AgeType>
+class Person2
+{
+public:
+    Person2(NameType name, AgeType age);
+    // {
+    //     this->m_Name = name;
+    //     this->m_Age = age;
+    // }
+
+    void showInfo();
+    // {
+    //     cout << "Name: " << this->m_Name << " Age: " << this->m_Age<<endl;
+    // }
+
+    NameType m_Name;
+    AgeType m_Age;
+};
+
+template <class NameType, class AgeType>
+Person2<NameType, AgeType>::Person2(NameType name, AgeType age)
+{
+    this->m_Name = name;
+    this->m_Age = age;
+}
+
+template <class NameType, class AgeType>
+void Person2<NameType, AgeType>::showInfo()
+{
+    cout << "Name: " << this->m_Name << " Age: " << this->m_Age << endl;
+}
+
+void printPerson(Person<string, int> &p)
+{
+    p.showInfo();
+}
+
+void test01()
+{
     Person<string, int> p1("Tom", 28);
-    p1.showInfo();
+    //p1.showInfo();
+    printPerson(p1);
+
+    Person2<string, int> p2("Bob", 28);
+    p2.showInfo();
 }
 
 int main()
