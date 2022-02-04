ae9675f00252e6720925d7e3fe3b2fe8cb11c9d8
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-04 15:55:09 +0800

    vector swap()
    巧用vector<int>(v).swap(v);来收缩内存

--- C++/STL初识/vectorInsertErase.cpp
+++ C++/STL初识/vectorInsertErase.cpp
@@ -38,11 +38,62 @@ void test01()
     //清空
     v.erase(v.begin(), v.end());
     printVector(v);
+}
+
+void test02(){
+    vector<int> v;
+    for(int i = 0; i < 10; i++){
+        v.push_back(i);
+    }
+
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i]<<" ";
+        cout<<v.at(i)<<" ";
+    }
+    cout<<endl;
+
+    cout << v.front()<<" "<<v.back()<<endl;
+}
+
+void test03(){
+    vector<int> v;
+    for(int i = 0; i < 10; i++){
+        v.push_back(i);
+    }
+    printVector(v);
+
+    vector<int> v2;
+    for (int i =10; i>0; i--){
+        v2.push_back(i);
+    }
+    printVector(v2);
+
+    v.swap(v2);
+    printVector(v);
+    printVector(v2);
+}
+
+void test04(){
+    vector<int> v;
+    for (int i = 0; i < 100000; i++){
+        v.push_back(i);
+    }
+
+    cout <<"v capacity "<< v.capacity()<< endl;
+    cout <<"v size "<< v.size()<< endl;
     
+    v.resize(3);
+    cout <<"v capacity "<< v.capacity()<< endl;
+    cout <<"v size "<< v.size()<< endl;
+
+    //swap() to reduce memory
+    vector<int> (v).swap(v);
+    cout <<"v capacity "<< v.capacity()<< endl;
+    cout <<"v size "<< v.size()<< endl;
 }
 
 int main()
 {
-    test01();
+    test04();
     return 0;
 }
