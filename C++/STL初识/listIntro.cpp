fadf9da4082a7b353f2390a038dda197aa4beaa7
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-15 16:58:47 +0800

    List container swap(), assign()

--- C++/STL初识/listIntro.cpp
+++ C++/STL初识/listIntro.cpp
@@ -29,10 +29,33 @@ void test01()
 
     list<int> l4(10, 1999);
     printList(l4);
+
+    list<int> l5, l6;
+    l5 = l2;
+
+    l6.assign(l5.begin(), l5.end());
+    printList(l6);
+}
+
+void test02(){
+    list<int> l1;
+    l1.push_back(10);
+    l1.push_back(20);
+    l1.push_back(30);
+    l1.push_back(40);
+    l1.push_back(50);
+
+    list<int> l4(10, 1999);
+    printList(l1);
+    printList(l4);
+    
+    l1.swap(l4);
+    printList(l1);
+    printList(l4);
 }
 
 int main()
 {
-    test01();
+    test02();
     return 0;
 }
