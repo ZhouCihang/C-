ce5c303b98c488b4c1748489c5fc25a0d02e40fc
ZhouCihang <matthewzhou1128@gmail.com>
2022-02-15 17:10:18 +0800

    list container insert(), erase(), remove(), clear()

--- C++/STL初识/listIntro.cpp
+++ C++/STL初识/listIntro.cpp
@@ -60,8 +60,43 @@ void test02(){
     }
 }
 
+void test03(){
+    list<int> l1;
+    l1.push_back(10);
+    l1.push_back(20);
+    l1.push_back(30);
+    
+    l1.push_front(100);
+    l1.push_front(200);
+    l1.push_front(300);
+    printList(l1);
+    l1.pop_back();
+    printList(l1);
+    l1.pop_front();
+    printList(l1);
+    
+
+    l1.insert(++l1.begin(), 1000);
+    printList(l1);
+
+    l1.erase(++l1.begin());
+    printList(l1);
+
+    l1.push_back(10000);
+    l1.push_back(10000);
+    l1.push_back(10000);
+    l1.push_back(10000);
+    printList(l1);
+
+    l1.remove(10000);
+    printList(l1);
+
+    l1.clear();
+    printList(l1);
+}
+
 int main()
 {
-    test02();
+    test03();
     return 0;
 }
